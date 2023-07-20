cea3f36dca6b008fff146cc9efa98f39f814ba83
Jean Toapanta <jptoapanta4@espe.edu.ec>
2023-07-19 19:52:29 -0500

    Config project
    Configuración del proyecto

---
+++ examen2/src/test/java/ec/edu/espe/arquitectura/examen2/Examen2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.examen2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
